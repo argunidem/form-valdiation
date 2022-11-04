3cacc8a5f9a1df4e4773cb96a0c2e33f81247635
argunidem <argun.idem@hotmail.com>
2022-11-04 18:36:13 +0100

    markup and styles

---
+++ script.js
@@ -0,0 +1,8 @@
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const confirmation = document.getElementById('confirmation');
+
+document.querySelector('form').addEventListener('click', (e) => {
+  e.preventDefault();
+});
